bc0deba87fb74cef41198b1a56d6daa9b9788b9e
NavaLeib <navaleib.research@gmail.com>
2022-06-30 09:27:57 -0400

    Add Passaging by %Y and Days
    Added a function which passage every m days a Y% of the population
    regardless of the population size and/or the generation
    
    Added for loop to vary:
    days before  passage
    %Y to passage
    initial clone size

--- analysis.py
+++ analysis.py
@@ -62,7 +62,8 @@ class Analysis(Model):
         plt.xlabel('generation')
         plt.ylabel('ratio')
         plt.legend()
-        plt.show()
+        #plt.show()
+        plt.savefig('figure_ratio_generation_many_mutants.pdf')
         pass
 
     def figure_hist_many_mutants(self, number_of_mutations, generations_to_plot):
@@ -108,7 +109,7 @@ class Analysis(Model):
             axs[i].set_yscale('log')
             axs[i].set_xscale('log')
             axs[i].set_title("$\\bf{1,12,17,20}$: "+str(round(ratio[0][gen_num],2)) + "; $\\bf{1q}$: "
-                             +str(round(ratio[0][gen_num],2)), fontsize=20)
+                             +str(round(ratio[1][gen_num],2)), fontsize=20)
 
             #plt.show()
 
@@ -142,7 +143,8 @@ class Analysis(Model):
             plt.setp(axs[i].get_xticklabels(), fontsize=20)
             plt.setp(axs[i].get_yticklabels(), fontsize=20)
         plt.gca().legend(('_nolegend_','_nolegend_','_nolegend_','1,12,17,20','1q','normal'),fontsize=16)
-        plt.show()
+        #plt.show()
+        plt.savefig('figure_hist_many_mutants.pdf')
 
         [gen1,gen2] = generations_to_plot
         Data1 = self.X_X[gen1, :] / (self.X_X[gen1, :].sum())
@@ -152,4 +154,126 @@ class Analysis(Model):
 
         print(stats.ks_2samp(Data1, Data2))
 
+        print(stats.ks_2samp(rP, curvey))
+
+        pass
+
+    def figure_hist_many_mutants_all_pass(self, number_of_mutations,generations_to_plot):
+        """
+        generate from data stacked histogram for different generation
+        :argument:
+        number_of_mutation = number of mutations specified in teh model (int)
+        generations_to_plot = array of generations to plot [gen_1,gen_2,....] (int, int,.. ) --> currently [gen1, gen2]
+        :return:
+        """
+
+        ratio=[]
+        for mut in range(number_of_mutations):
+            ratio.append([self.X_X[i, int(mut * self.number_of_species * self.mutant_percent):
+                                    int((mut+1) * self.number_of_species * self.mutant_percent)].sum(axis=0)
+                          / (self.X_X[i, :].sum(axis=0)) for i in range(self.gen_num + 1)])
+
+        fig, axs = plt.subplots(2,2)
+        fig.supxlabel('clone size (fraction of total cells)',fontsize=10)
+        fig.supylabel('clones counts',fontsize=10)
+
+        for (index,gen_num) in enumerate(generations_to_plot):
+            location=[(0,0),(0,1),(1,0),(1,1)]
+            (i,j)= location[index]
+
+            Data = self.X_X[gen_num,:]/(self.X_X[gen_num,:].sum())
+            Data = Data[Data>0]
+
+            min_data=min(Data)
+            max_data = max(Data)
+            #print(min_data)
+
+            P = ss.expon.fit(Data)
+            rX = np.linspace(min_data,max(Data), 1000)
+            rP = ss.expon.pdf(rX, *P)
+            axs[i,j].plot(rX, rP, color='blue')
+
+            bins = 10**(np.arange(np.log10(min_data),max(Data),0.1))
+            counts, bins, bars = axs[i,j].hist([Data[:int(self.number_of_species * self.mutant_percent/2)],
+                                                Data[int(self.number_of_species * self.mutant_percent/2):
+                                                     int(self.number_of_species * self.mutant_percent)],
+                                                Data[int(self.number_of_species * self.mutant_percent):]], density=True,
+                                               bins=10**(np.arange(-4.5,-1.5,0.1)),alpha=0.5 ,histtype='barstacked',
+                                               color=('orange','saddlebrown','blue'))
+            counts, bins, bars = axs[i,j].hist(Data, density=True,bins=10**(np.arange(-4.5,0,0.1)),alpha=0.0)
+            #plt.plot(bins[1:]-np.diff(bins)/2,counts ,'.')
+            axs[i,j].set_yscale('log')
+            axs[i,j].set_xscale('log')
+            axs[i,j].set_title("$\\bf{1,12,17,20}$: "+str(round(ratio[0][gen_num],2)) + "; $\\bf{1q}$: "
+                             +str(round(ratio[1][gen_num],2)),fontsize=10)
+
+
+
+            from scipy.optimize import curve_fit
+
+            x = bins[1:]-np.diff(bins)/2
+
+            def MLE(params,*args):
+                y= np.array(args,dtype=float)
+                a, k1, k2 = params
+                yPred = (1-a)*k1*np.exp(-k1*y) + a*k2*np.exp(-k2*y)
+                negLL = -np.sum(np.log(yPred))
+                return negLL
+
+            y = counts
+            import scipy.optimize
+            from scipy.optimize import minimize
+            bnd = ((0, 1), (10**2,10**5), (10**2, 10**5))
+            guess=np.array([1,1000,1000])
+            results = scipy.optimize.minimize(MLE, guess, args=Data,bounds=bnd,method= 'L-BFGS-B')
+
+            print(results.x) # This contains your three best fit parameters
+            a, k1, k2 = results.x
+            curvey = (1-a)*k1*np.exp(-k1*x) + a*k2*np.exp(-k2*x) # This is your y axis fit-line
+
+            axs[i,j].plot(x, curvey,color='darkorange')
+            axs[i,j].scatter(x,y,color='black')
+            axs[i,j].set_ylim(10**(-2),10**(5))
+            axs[i,j].set_xlim(bins[0], 10**(-2.5))
+            plt.legend(loc='best')
+            plt.setp(axs[i,j].get_xticklabels(),fontsize=10)
+            plt.setp(axs[i,j].get_yticklabels(),fontsize=10)
+
+            gen2 = gen_num
+            Data1 = self.X_X[1, :] / (self.X_X[1, :].sum())
+            Data1 = Data1[Data1 > 0]
+            Data2 = self.X_X[gen2, :] / (self.X_X[gen2, :].sum())
+            Data2 = Data2[Data2 > 0]
+
+            (ks,p)=stats.ks_2samp(Data1, Data2)
+
+            axs[i, j].set_title('H0: PDF(gen=1) = PDF (gen='+str(gen2)+'); p_value = ' +str(p))
+            print('between two measuremnts', stats.ks_2samp(Data1, Data2), 'gen=', gen2)
+
+            print('between two fitting curves', stats.ks_2samp(rP, curvey),'gen=', gen2)
+
+        plt.gca().legend(('_nolegend_','_nolegend_','_nolegend_','1,12,17,20','1q','normal'),fontsize=10)
+        plt.show()
+        #plt.savefig('figure_hist_many_mutants.pdf')
+
+
+
+        pass
+
+    def figure_number_clones(self, passaging, initial_mean_clone_size):
+        """
+        generate from data plot the number of clones in each passaging
+        :param passaging: int
+        :return:
+        """
+        num_non_zero = [((self.X_X[i, :])!=0).sum(axis=0) for i in range(self.gen_num + 1)]
+        print(num_non_zero)
+        plt.plot(num_non_zero[::passaging], 'o', label = str(initial_mean_clone_size) )
+        plt.xlabel('# passaging')
+        plt.legend()
+        plt.ylabel('# unique barcodes = # clones')
+        #plt.title('initial mean clone size' + str(initial_mean_clone_size) )
+        #plt.show()
+        print('figure_number_clones_init_cells_per_clone' + str(initial_mean_clone_size) + '.pdf')
+        plt.savefig('figure_number_clones_init_cells_per_clone' + str(initial_mean_clone_size) + '.pdf')
         pass
